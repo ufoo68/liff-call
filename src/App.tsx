ac4b96cb5235a7ccf8c4fd0a4a037e7124077458
ufoo68 <mitarou.yuutarou@gmail.com>
2020-07-24 20:48:05 +0900

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from 'react'
+import Peer from 'skyway-js'
+import { liff } from '@line/liff'
 
-function App() {
+const peer = new Peer({ key: process.env.REACT_APP_SKYWAY_KEY ?? '' })
+
+const App = () => {
+  const [myId, setMyId] = useState<string>('')
+  const [callId, setCallId] = useState<string>('')
+  const localVideo = useRef<HTMLVideoElement>(null)
+  const remoteVideo = useRef<HTMLVideoElement>(null)
+
+  useEffect(() => {
+
+    liff.init({ liffId: process.env.REACT_APP_LIFF_ID ?? '' }).then(() => {
+      if (!liff.isLoggedIn()) {
+        liff.login({})
+      }
+    })
+
+    peer.on('open', () => {
+      setMyId(peer.id)
+      navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(localStream => {
+        if (localStream) {
+          localVideo!.current!.srcObject = localStream
+        }
+      })
+    })
+
+    peer.on('call', mediaConnection => {
+      mediaConnection.answer(localVideo!.current!.srcObject as MediaStream)
+
+      mediaConnection.on('stream', async stream => {
+        remoteVideo!.current!.srcObject = stream
+      })
+    })
+  }, [])
+
+  const makeCall = () => {
+    const mediaConnection = peer.call(callId, localVideo!.current!.srcObject as MediaStream)
+    mediaConnection.on('stream', async stream => {
+      remoteVideo!.current!.srcObject = stream
+      await remoteVideo!.current!.play().catch(console.error)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <video width={400} autoPlay muted playsInline ref={localVideo} />
+        <video width={400} autoPlay muted playsInline ref={remoteVideo} />
+      </div>
+      <div>{myId}</div>
+      <div>
+        <input value={callId} onChange={e => setCallId(e.target.value)}></input>
+        <button onClick={makeCall}>発信</button>
+      </div>
+      <div></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
